d81a9a9c8c506d243959363b60f60a00b994b0fe
SalemA1125 <andreavaldezsalem@gmail.com>
2024-10-30 20:47:03 +0800

    classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = prompt("DSA:");
+let classSchedule = prompt("(8-9:30 am, MW):");
+let classroom = prompt("ComLab B:");
+let classInstructor = prompt("Mr. Cliff Owen Pascua:");
+let studentName = prompt("Andrea Salem:");
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}`);
