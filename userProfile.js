457ce70f1efde879cffc5e5e2064b323f44368ac
SalemA1125 <andreavaldezsalem@gmail.com>
2024-10-30 20:13:59 +0800

    userProfile.js

--- userProfile.js
+++ userProfile.js
@@ -3,15 +3,6 @@ let age = parseInt(prompt("19:"));
 let favNumber = parseInt(prompt("25:"));
 let favColor = prompt("Blue:");
 
-// Input validation (optional, but good practice):
-while (isNaN(age) || age <= 0) {
-  age = parseInt(prompt("Invalid age. Please enter a positive number:"));
-}
-while (isNaN(favNumber)) {
-  favNumber = parseInt(prompt("Invalid favorite number. Please enter a number:"));
-}
-
-
 console.log("User Profile:");
 console.log("Full Name:", fullName);
 console.log("Age:", age);
